aec6bbc42c22dcf0d6aab861259a39e731c2783d
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-05-05 16:43:47 +0100

    Update Campaign.tsx

--- src/desktop/pages/about/Campaign.tsx
+++ src/desktop/pages/about/Campaign.tsx
@@ -13,9 +13,8 @@ export class Campaign extends React.PureComponent {
             Scotland's Instagrammer community voiced out its concern over the Scottish outdoors in
             Spring 2017. The community was concerned about the degradation of the popular Scottish
             nature sites due to littering and over-use by visitors. Together, Scotland's
-            Instagrammers signed a charter, which addressed the issues mentioned above and agreed to
-            promote responsible roaming outdoors, as well as promote the Scottish Outdoor Access
-            Code itself.
+            Instagrammers signed a charter, which addressed these issues and agreed to promote the
+            Scottish Outdoor Access Code as well as enjoying Scotland's beauty responsibly.
           </p>
         </section>
         <section>
