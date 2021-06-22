6583bd5c6d15ccba50f8675543a1700e6b00ee7b
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-06-22 17:35:23 +0100

    helmet configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react';
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import { AppState, ViewMode } from './AppState';
 import { DesktopApp } from './desktop/DesktopApp';
@@ -31,7 +32,25 @@ export class App extends React.PureComponent {
         break;
     }
 
-    return <>{app}</>;
+    return (
+      <>
+        <Helmet>
+          <title>RoamResponsibly</title>
+          <meta property={'og:title'} content={'RoamResponsibly'} />
+          <meta
+            property={'og:description'}
+            content={"Scotland's Instagrammers campaign to #RoamResponsibly"}
+          />
+          <meta property={'og:url'} content={'https://www.roamresponsibly.com/#/about/campaign'} />
+          <meta property={'og:image'} content={'../../../dist/assets/bannerImages/about.JPG'} />
+          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+          <meta http-equiv='X-UA-Compatible' content='ie=edge' />
+          <meta property={'og:type'} content={'website'} />
+          <meta property={'og:locale'} content={'en_GB'} />
+        </Helmet>
+        {app}
+      </>
+    );
   }
 
   private readonly onHistoryChange = () => {
