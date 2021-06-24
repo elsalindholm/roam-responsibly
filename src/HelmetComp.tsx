165b4aee4033108c202493d70b15611b0ec775c3
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-06-24 09:51:15 +0100

    testing meta tags

--- src/HelmetComp.tsx
+++ src/HelmetComp.tsx
@@ -13,14 +13,14 @@ export class HelmetComp extends React.PureComponent<HelmetCompProps> {
     return (
       <Helmet>
         <title>{pageMetaData.title}</title>
-        <link rel="canonical" href={pageMetaData.link} />
-        <meta name="description" content={pageMetaData.description} />
-        <meta property={"og:title"} content={pageMetaData.ogTitle} />
-        <meta property={"og:description"} content={pageMetaData.ogDescription}/>
-        <meta property={"og:url"} content={pageMetaData.ogUrl} />
-        <meta property={"og:image"} content={pageMetaData.ogImage} />
-        <meta property={"og:type"} content={pageMetaData.ogType} />
-        <meta property={"og:locale"} content={"en_GB"}/>
+        <link rel={'canonical'} href={pageMetaData.link} />
+        <meta name={'description'} content={pageMetaData.description} />
+        <meta property={'og:title'} content={pageMetaData.ogTitle} />
+        <meta property={'og:description'} content={pageMetaData.ogDescription} />
+        <meta property={'og:url'} content={pageMetaData.ogUrl} />
+        <meta property={'og:image'} content={pageMetaData.ogImage} />
+        <meta property={'og:type'} content={pageMetaData.ogType} />
+        <meta property={'og:locale'} content={'en_GB'} />
       </Helmet>
     );
   }
